3a9cf03607517312be6c909029f459df8dfd7c35
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-03-07 21:53:57 -0300

    Added Next Auth authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,17 @@
 import { FaGithub } from "react-icons/fa"
 import { FiX } from "react-icons/fi"
+import { signIn, signOut, useSession } from "next-auth/react"
+
 import styles from "./styles.module.scss"
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    return isUserLoggedIn ? (        
+    return session ? (        
         <button
         type="button"
         className={styles.signInButton}
+        onClick={() => {signOut()}}
         >
             <FaGithub color="#84d361" />
             Murilo Pinheiro
@@ -18,6 +21,7 @@ export function SignInButton(){
         <button
         type="button"
         className={styles.signInButton}
+        onClick={() => {signIn('github')}}
         >
             <FaGithub color="#eba417" />
             Sign in with GitHub
