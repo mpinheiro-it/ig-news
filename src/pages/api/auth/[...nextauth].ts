e73db24f2cba1c70aeb4263672104feb517df776
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-03-08 21:18:18 -0300

    created faunaDB connection

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,9 +14,26 @@ export default NextAuth({
       authorization: {
             params: {
                 scope: 'read:user',
-            },
-        },
-    }),
-    // ...add more providers here
+            }},        
+    }),    
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user;
+
+      try{
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: { email }}
+          )
+        )
+        return true
+      }  catch {
+        return false
+      }     
+    },
+  }
+
 })
