dedad746b24c7041cbfd9fc243aecb74b0a3a521
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-03-11 21:21:57 -0300

    fixed duplicate stripe customer issue

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,20 +1,63 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import {query as q} from 'faunadb';
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+    ref: {
+        id: string
+    }
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   
+    
+    const session = await getSession({ req })
+
     if (req.method == 'POST'){
 
-        const session = await getSession({ req })
+    //getting the session user from the fauna database
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)                    
+                )
+            )
+        )
+
+        let customerId = user.data.stripe_customer_id //from the DB
+
+            if (!customerId) {
+                const stripeCustomer = await stripe.customers.create({
+                    email: session.user.email,
+                    //metadata
+                })
+
+                //adding the stripe customer into the database as well
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {//info sendo salva no DB
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )  
+
+            customerId = stripeCustomer.id          
+        }
+               
+       
         
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            //metadata
-        })
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
@@ -27,6 +70,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         })
 
         return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+
     } else {
         res.setHeader('Allow','POST')
         res.status(405).end('Method not allowed')
