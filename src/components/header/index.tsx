902f898cc318ecb761773951d21bca6f59ebd813
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-03-22 21:10:40 -0300

    header links action

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,26 @@
 import { SignInButton } from "../SignInButton";
+import Link  from "next/link"
 import styles from "./styles.module.scss"
+import { useRouter } from "next/router";
 
 export function Header() {
+
+    const { asPath } = useRouter()
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
 
                 <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
+                    <Link href="/">
+                        <a className={asPath == "/" ? styles.active : ""}> Home</a>
+                    </Link>
+
+                    <Link href="/posts">
+                        <a className={asPath == "/posts" ? styles.active : ""}> Posts</a>
+                    </Link>
+                    
                 </nav>
                 <SignInButton />
             </div>       
