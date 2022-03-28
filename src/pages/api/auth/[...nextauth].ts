405aa8560ba761b82ee6047a83bd9d1714ab254d
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-03-28 20:21:36 -0300

    subscription validation

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,8 @@ import GithubProvider from "next-auth/providers/github"
 
 import { fauna } from '../../../services/fauna'
 
-export default NextAuth({
+export default NextAuth({  
+
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -17,8 +18,44 @@ export default NextAuth({
             }},        
     }),    
   ],
-
   callbacks: {
+    async session({ session }) {   
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([         
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])          
+          )
+        )
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      }
+      catch{
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+    },
+          
     async signIn({ user, account, profile}) {
       const { email } = user;
 
